c3358f3c68db074c9a47acc7ee102c43bc8df6a2
arsay <arsay@foxmail.com>
2018-05-14 18:16:15 +0800

    feat : all functionality basically done;

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # AutoPublisher
 AutoPublisher tool in node.js
+
+## How to run
+
+1. Confirm you installed Node.js & npm in your system.
+    `node -v`
+    `npm -v`
+
+2. Install dependencies
+    `npm install`
+
+3. Copy following files to the 'source' directory,
+
+    `STACofig-ios.txt`
+    `STACofig-android.txt`
+    `www.zip`
+    `www-ios.zip`  (not required)
+    `www-android.zip`  (not required)
+
+
+4. Run the project
+    `npm start`
--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 var crypto = require('crypto');
 var fs = require('fs');
 
-var hash = crypto.createHash('sha512');
-
-fs.readdir('./',function (err1,files) {
-    if(!(files.indexOf('source')<0)){
-        (files.indexOf('target')<0) ? fs.mkdir('target',function (err2) {
-            if(err2) throw err2 || '--- mkdir target failed! ---';
-            console.log('*** target ***,mkdir successfully!');
-        }): console.log('target folder exists!');
-    }else{
-        throw '--- source does not exist! ---';
-    }
-});
+var hash = null;
 
 var iosConf = fs.readFileSync('./source/STAConfig-ios.txt');
 var iosConfigJSON = JSON.parse(iosConf.toString());
@@ -20,31 +9,82 @@ var androidConf = fs.readFileSync('./source/STAConfig-android.txt');
 var androidConfigJSON = JSON.parse(androidConf.toString());
 
 
+// 清空target
+fs.exists('./target',function (exists) {
+    if(exists) {
+        deleteFolder('./target',function () {
+            fs.mkdirSync('./target',function (err) { if(err) throw err; });
+        });
+    }else{
+        fs.mkdirSync('./target',function (err) { if(err) throw err; });
+    }
 
-var www_file = fs.createReadStream('./source/www.zip');
-var www_hashcode = null;
-
-www_file.on('data', hash.update.bind(hash));
-www_file.on('end', function () {
-    www_hashcode = hash.digest('hex').toUpperCase();
-    console.log(www_hashcode, typeof www_hashcode, typeof www_file);
-    writeWWWConf(www_hashcode);
 });
 
-function writeWWWConf(hashcode) {
-    iosConfigJSON.webresources[1].hashcode = hashcode;
-    fs.writeFile('./target/STAConfig-ios.txt',JSON.stringify(iosConfigJSON),function (err) {
-        if(err) {
-            throw err;
-        }
+
+fs.exists('./source/www.zip', function (exists) {
+    if(exists){
+        hash = crypto.createHash('sha512');
+        hash.update(fs.readFileSync('./source/www.zip'));
+        var www_hashcode = hash.digest('hex').toUpperCase();
+        //set conf
+        iosConfigJSON.webresources[1].hashcode = www_hashcode;
+        androidConfigJSON.webresources[1].hashcode = www_hashcode;
+        //复制到target
+        copyfile('./source/www.zip','./target/www.zip');
+    }
+    addConfigFile();
+});
+fs.exists('./source/www-ios.zip', function (exists) {
+    if(exists){
+        hash = crypto.createHash('sha512');
+        hash.update(fs.readFileSync('./source/www-ios.zip'));
+        var ios_hashcode = hash.digest('hex').toUpperCase();
+        //set conf
+        iosConfigJSON.webresources[0].hashcode = ios_hashcode;
+        //复制到target
+        copyfile('./source/www-ios.zip','./target/www-ios.zip');
+    }
+    addConfigFile();
+});
+fs.exists('./source/www-android.zip', function (exists) {
+    if(exists){
+        hash = crypto.createHash('sha512');
+        hash.update(fs.readFileSync('./source/www-android.zip'));
+        var android_hashcode = hash.digest('hex').toUpperCase();
+        //set conf
+        androidConfigJSON.webresources[0].hashcode = android_hashcode;
+        //复制到target
+        copyfile('./source/www-android.zip','./target/www-android.zip');
+    }
+    addConfigFile();
+});
+
+
+function addConfigFile() {
+
+    fs.writeFileSync('./target/STAConfig-ios.txt',JSON.stringify(iosConfigJSON),function (err) {
+        if(err) { throw err; }
         console.log("The file was saved! ***** ios *****");
     });
 
-    androidConfigJSON.webresources[1].hashcode = hashcode;
-    fs.writeFile('./target/STAConfig-android.txt',JSON.stringify(androidConfigJSON),function (err) {
-        if(err) {
-            throw err;
-        }
+    fs.writeFileSync('./target/STAConfig-android.txt',JSON.stringify(androidConfigJSON),function (err) {
+        if(err) { throw err; }
         console.log("The file was saved! ***** android *****");
     });
+}
+
+function deleteFolder(path,callback) {
+    var files = fs.readdirSync(path);
+    files.forEach(function(file, index) {
+        var curPath = path + "/" + file;
+        fs.unlinkSync(curPath);
+        console.log(curPath);
+    });
+    console.log('delete done')
+    callback;
+};
+
+function copyfile(src,dir) {
+    fs.writeFileSync(dir,fs.readFileSync(src));
 }
